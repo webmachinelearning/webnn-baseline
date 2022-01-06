81cf976e7b19b368dbe4aeaeb4b2edc92085ef82
BruceDai <feng.dai@intel.com>
2022-01-06 18:04:18 +0800

    Refactor relu.

--- src/relu.js
+++ src/relu.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import {Tensor} from './tensor.js';
+import {unary} from './unary.js';
 
 /**
  * Compute the rectified linear function of the input tensor.
@@ -8,11 +8,5 @@ import {Tensor} from './tensor.js';
  * @return {Tensor}
  */
 export function relu(input) {
-  const output = new Tensor(input.shape);
-  for (let i = 0; i < output.size; ++i) {
-    const x = input.getValueByIndex(i);
-    const y = Math.max(0, x);
-    output.setValueByIndex(i, y);
-  }
-  return output;
+  return unary(input, (x) => Math.max(0, x));
 }
