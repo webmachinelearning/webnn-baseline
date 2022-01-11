f88779233ec3f65f10ac454fc7a4f846cb25fdf7
BruceDai <feng.dai@intel.com>
2022-01-11 11:34:34 +0800

    Implement split.

---
+++ src/split.js
@@ -0,0 +1,47 @@
+'use strict';
+
+import {slice} from './slice.js';
+
+/**
+ * Split the input tensor into a number of sub tensors along the given axis.
+ * @param {Tensor} input
+ * @param {Array|Number} splits
+ * @param {MLSplitOptions} options
+ * @return {Array.<Tensor>}
+ */
+export function split(input, splits, options = {}) {
+  const outputs = [];
+  let sliceSizes = [];
+  const rank = input.rank;
+  let axis = 0;
+  if (options.axis !== undefined) {
+    if (!Number.isInteger(options.axis)) {
+      throw new Error(`The axis ${options.axis} should be an integer.`);
+    }
+    if (options.axis >= rank || options.axis < -rank) {
+      throw new Error(`The axis ${options.axis} should be in the interval [${-rank}, ${rank}).`);
+    }
+    axis = options.axis;
+  }
+  if (typeof splits === 'number') {
+    if (!Number.isInteger(splits) || splits <= 0) {
+      throw new Error(`Invalid splits ${splits}, it should be a positive integer.`);
+    }
+    if (input.shape[axis] % splits !== 0) {
+      throw new Error(`The splits ${splits} must evenly divide the dimension size ` +
+        `${input.shape[axis]} of input along options.axis ${axis}.`);
+    }
+    sliceSizes = new Array(splits).fill(input.shape[axis] / splits);
+  } else if (splits instanceof Array) {
+    if (!splits.every((v) => Number.isInteger(v) && v > 0)) {
+      throw new Error(`Invalid splits ${splits}, it should be an Array of positive integers.`);
+    }
+    sliceSizes = splits.slice();
+  }
+  let start = 0;
+  for (const size of sliceSizes) {
+    outputs.push(slice(input, [start], [size], {axes: [axis]}));
+    start += size;
+  }
+  return outputs;
+}
