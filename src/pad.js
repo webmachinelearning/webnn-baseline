2645b5a2db524c97310c0da5f58ff51fcdd42573
BruceDai <feng.dai@intel.com>
2023-03-28 14:22:57 +0800

    Implement pad op fixes #15

---
+++ src/pad.js
@@ -0,0 +1,109 @@
+'use strict';
+
+import {Tensor} from './lib/tensor.js';
+
+/**
+ * Get mapped location from source tensor.
+ * @param {Array} location
+ * @param {Array} inputShape
+ * @param {Array} beginningPadding
+ * @param {MLPaddingMode} mode
+ * @return {Array} mapped location of source tensor
+ */
+function getMappedLocation(location, inputShape, beginningPadding, mode) {
+  const mappedLocation = location.slice();
+  const length = location.length;
+  if (mode === 'edge') {
+    for (let i = 0; i < length; i++) {
+      if (location[i] < beginningPadding[i]) {
+        mappedLocation[i] = 0;
+      } else if (location[i] >= beginningPadding[i] + inputShape[i]) {
+        mappedLocation[i] = inputShape[i] - 1;
+      } else {
+        mappedLocation[i] -= beginningPadding[i];
+      }
+    }
+  } else {
+    // reflection mode or symmetric mode
+    const offset = mode === 'symmetric' ? 1 : 0;
+    for (let i = 0; i < length; i++) {
+      if (mappedLocation[i] < beginningPadding[i]) {
+        // mappedLocation[i] =
+        //    beginningPadding[i] + (beginningPadding[i] - mappedLocation[i]) -
+        //    beginningPadding[i] - offset;
+        mappedLocation[i] = beginningPadding[i] - mappedLocation[i] - offset;
+      } else if (mappedLocation[i] >= beginningPadding[i] + inputShape[i]) {
+        // mappedLocation[i] =
+        //    beginningPadding[i] + inputShape[i] - 1 -
+        //    (mappedLocation[i] - (beginningPadding[i] + inputShape[i] -1)) -
+        //    beginningPadding[i] + offset;
+        mappedLocation[i] =
+            beginningPadding[i] + inputShape[i] * 2 - mappedLocation[i] - 2 + offset;
+      } else {
+        mappedLocation[i] -= beginningPadding[i];
+      }
+    }
+  }
+  return mappedLocation;
+}
+
+/**
+ * Update element value of destination tensor.
+ * @param {Number} index
+ * @param {Tensor} source
+ * @param {Tensor} destination
+ * @param {Array} beginningPadding
+ * @param {MLPaddingMode} mode
+ * @param {Number} value
+ * @return {Tensor}
+ */
+function updateOutput(index, source, destination, beginningPadding, mode, value) {
+  const inputShape = source.shape;
+  const location = destination.locationFromIndex(index);
+  const length = location.length;
+  let bFillPadding = false;
+  for (let j = 0; j < length; j++) {
+    if (location[j] < beginningPadding[j] || location[j] >= beginningPadding[j] + inputShape[j]) {
+      bFillPadding = true;
+      break;
+    }
+  }
+  let result;
+  if (bFillPadding) {
+    if (mode === 'constant') {
+      result = value;
+    } else if (mode === 'edge' || mode === 'reflection' || mode === 'symmetric') {
+      const targetLocation = getMappedLocation(location, inputShape, beginningPadding, mode);
+      result = source.getValueByLocation(targetLocation);
+    }
+  } else {
+    const inputLocation = location.map((v, d) => v - beginningPadding[d]);
+    result = source.getValueByLocation(inputLocation);
+  }
+  destination.setValueByIndex(index, result);
+  return destination;
+}
+
+/**
+ * Inflate the tensor with constant or mirrored values on the edges.
+ * @param {Tensor} input
+ * @param {Array} beginningPadding
+ * @param {Array} endingPadding
+ * @param {MLPadOptions} [options]
+ * @return {Tensor}
+ */
+export function pad(
+    input,
+    beginningPadding,
+    endingPadding,
+    {
+      mode='constant',
+      value=0,
+    } = {}) {
+  const outputShape = input.shape.map((v, i) => v + beginningPadding[i] + endingPadding[i]);
+  let output = new Tensor(outputShape);
+  for (let i = 0; i < output.size; ++i) {
+    output = updateOutput(i, input, output, beginningPadding, mode, value);
+  }
+  return output;
+}
