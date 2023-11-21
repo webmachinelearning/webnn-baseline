337b0f65ae9f51f24230bec96772bcdccf4bcbd0
Mei <shanxing.mei@intel.com>
2023-11-21 16:45:49 +0800

    modified the format of where_test.js

--- test/where_test.js
+++ test/where_test.js
@@ -17,19 +17,31 @@ describe('test where', function() {
   it('where', function() {
     const condition = {
       shape: [2, 3],
-      data: [1, 1, 0, 0, 1, 0],
+      data: [
+        1, 1, 0,
+        0, 1, 0,
+      ],
     };
     const inputA = {
       shape: [2, 3],
-      data: [1, 2, 3, 4, 5, 64],
+      data: [
+        1, 2, 3,
+        4, 5, 64,
+      ],
     };
     const inputB = {
       shape: [2, 3],
-      data: [6, 3, 5, 7, 8, 0],
+      data: [
+        6, 3, 5,
+        7, 8, 0,
+      ],
     };
     const expected = {
       shape: [2, 3],
-      data: [1, 2, 5, 7, 5, 0],
+      data: [
+        1, 2, 5,
+        7, 5, 0,
+      ],
     };
     testWhere(condition, inputA, inputB, expected);
   });
@@ -41,15 +53,24 @@ describe('test where', function() {
     };
     const inputA = {
       shape: [2, 3],
-      data: [1, 2, 3, 4, 5, 64],
+      data: [
+        1, 2, 3,
+        4, 5, 64,
+      ],
     };
     const inputB = {
       shape: [2, 3],
-      data: [7, 8, 9, 10, 11, 12],
+      data: [
+        7, 8, 9,
+        10, 11, 12,
+      ],
     };
     const expected = {
       shape: [2, 3],
-      data: [1, 2, 9, 4, 5, 12],
+      data: [
+        1, 2, 9,
+        4, 5, 12,
+      ],
     };
     testWhere(condition, inputA, inputB, expected);
   });
@@ -57,11 +78,17 @@ describe('test where', function() {
   it('where broadcast condition2d×A2d×B1d', function() {
     const condition = {
       shape: [2, 3],
-      data: [1, 1, 0, 0, 0, 1],
+      data: [
+        1, 1, 0,
+        0, 0, 1,
+      ],
     };
     const inputA = {
       shape: [2, 3],
-      data: [1, 2, 3, 4, 5, 64],
+      data: [
+        1, 2, 3,
+        4, 5, 64,
+      ],
     };
     const inputB = {
       shape: [3],
@@ -69,7 +96,10 @@ describe('test where', function() {
     };
     const expected = {
       shape: [2, 3],
-      data: [1, 2, 9, 7, 8, 64],
+      data: [
+        1, 2, 9,
+        7, 8, 64,
+      ],
     };
     testWhere(condition, inputA, inputB, expected);
   });
