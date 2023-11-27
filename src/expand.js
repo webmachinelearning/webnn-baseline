3feda156da756711398c55af6f64847780de8f43
Mei <shanxing.mei@intel.com>
2023-11-27 11:14:49 +0800

    modified expand.js and add more tests

--- src/expand.js
+++ src/expand.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import {broadcast} from './lib/broadcast.js';
+import {broadcast, getBroadcastShape} from './lib/broadcast.js';
+import {Tensor} from '../src/lib/tensor.js';
 
 /**
  * Expand any dimension of size 1 of the input tensor to a
@@ -10,5 +11,13 @@ import {broadcast} from './lib/broadcast.js';
  * @return {Tensor}
  */
 export function expand(input, newShape) {
-  return broadcast(input, newShape);
+  if (input.shape.length === 0) {
+    const inputReshape = new Tensor([1], input.data);
+    const outputShape = getBroadcastShape(inputReshape.shape, newShape);
+    return broadcast(inputReshape, outputShape);
+  } else {
+    const inputReshape = new Tensor(input.shape, input.data);
+    const outputShape = getBroadcastShape(inputReshape.shape, newShape);
+    return broadcast(inputReshape, outputShape);
+  }
 }
--- test/expand_test.js
+++ test/expand_test.js
@@ -12,30 +12,55 @@ describe('test expand', function() {
     utils.checkValue(outputTensor, expected.data);
   }
 
-  it('expand changed dimensions', function() {
+  it('expand a 3D input with a 4D newShape to a 4D output.', function() {
     const input = {
-      shape: [2, 3],
+      shape: [2, 1, 4],
       data: [
-        1, 1, 0,
-        0, 1, 0,
+        1, 2, 3, 4, 5, 6, 7, 8,
       ],
     };
 
-    const newShape = [2, 2, 3];
+    const newShape = [5, 1, 3, 4];
 
     const expected = {
-      shape: [2, 2, 3],
+      shape: [5, 2, 3, 4],
       data: [
-        1, 1, 0,
-        0, 1, 0,
-        1, 1, 0,
-        0, 1, 0,
+        1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
+        5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8,
+        1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
+        5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8,
+        1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
+        5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8,
+        1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
+        5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8,
+        1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
+        5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8,
       ],
     };
     testExpand(input, newShape, expected);
   });
 
-  it('expand unchanged dimensions', function() {
+  it('expand a 3D input with a 2D newShape to a 3D output', function() {
+    const input = {
+      shape: [2, 1, 4],
+      data: [
+        1, 2, 3, 4, 5, 6, 7, 8,
+      ],
+    };
+
+    const newShape = [3, 1];
+
+    const expected = {
+      shape: [2, 3, 4],
+      data: [
+        1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
+        5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8,
+      ],
+    };
+    testExpand(input, newShape, expected);
+  });
+
+  it('expand a 2D input with a 2D newShape to a 2D output', function() {
     const input = {
       shape: [3, 1],
       data: [
@@ -51,6 +76,47 @@ describe('test expand', function() {
         1, 1, 1, 1,
         2, 2, 2, 2,
         3, 3, 3, 3,
+      ],
+    };
+    testExpand(input, newShape, expected);
+  });
+
+  it('expand a 0D input with a 2D newShape to a 2D output.', function() {
+    const input = {
+      shape: [],
+      data: [
+        6,
+      ],
+    };
+
+    const newShape = [2, 3];
+
+    const expected = {
+      shape: [2, 3],
+      data: [
+        6, 6, 6,
+        6, 6, 6,
+      ],
+    };
+    testExpand(input, newShape, expected);
+  });
+
+  it('expand a 2D input with a 0D newShape to a 2D output.', function() {
+    const input = {
+      shape: [2, 3],
+      data: [
+        1, 2, 3,
+        4, 5, 6,
+      ],
+    };
+
+    const newShape = [];
+
+    const expected = {
+      shape: [2, 3],
+      data: [
+        1, 2, 3,
+        4, 5, 6,
       ],
     };
     testExpand(input, newShape, expected);
