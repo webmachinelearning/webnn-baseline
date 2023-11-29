69d45f89f987065ca7dd56921486729a8a6942fc
mei1127 <shanxing.mei@intel.com>
2023-11-29 13:35:10 +0800

    implement erf

--- src/unary.js
+++ src/unary.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import {Tensor} from './lib/tensor.js';
+import {Scalar, Tensor} from './lib/tensor.js';
+import {add, div, mul, sub} from '../src/binary.js';
 
 /**
  * Compute the element-wise unary operation for input tensor.
@@ -30,3 +31,25 @@ export const tan = (input) => unary(input, Math.tan);
 export const copy = (input) => unary(input, (x) => x);
 export const reciprocal = (input) => unary(input, (x) => 1 / x);
 export const sqrt = (input) => unary(input, Math.sqrt);
+export const sign = (input) => unary(input, Math.sign);
+
+export const erf = (input) => {
+  /**
+  *reference 1:https://en.wikipedia.org/wiki/Error_function
+  *reference 2:https://github.com/tensorflow/tfjs/blob/master/tfjs-backend-webgl/src/kernels/Erf.ts
+  */
+  const a1 = new Scalar(0.254829592);
+  const a2 = new Scalar(-0.284496736);
+  const a3 = new Scalar(1.421413741);
+  const a4 = new Scalar(-1.453152027);
+  const a5 = new Scalar(1.061405429);
+  const p = new Scalar(0.3275911);
+  const ones = new Scalar(1);
+  input = input.shape.length === 0 ? new Scalar(input.data) : input;
+  const signInput = sign(input);
+  input = abs(input);
+  const t = div(ones, add(ones, mul(p, input)));
+  const y = mul(add(mul(add(mul(add(mul(add(mul(a5, t), a4), t), a3), t), a2), t), a1), t);
+  const result = mul(signInput, sub(ones, mul(y, exp(neg(mul(input, input))))));
+  return result;
+};
