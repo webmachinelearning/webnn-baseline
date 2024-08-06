8a5ab0f48496cb394a62ce9e387ee2e459a791a2
mei1127 <shanxing.mei@intel.com>
2024-08-06 10:14:15 +0800

    Remove softplus parameter steepness

--- test/softplus_test.js
+++ test/softplus_test.js
@@ -5,9 +5,9 @@ import {Tensor} from '../src/lib/tensor.js';
 import * as utils from './utils.js';
 
 describe('test softplus', function() {
-  function testElu(inputShape, inputValue, expected, options = {}) {
+  function testElu(inputShape, inputValue, expected) {
     const inputTensor = new Tensor(inputShape, inputValue);
-    const outputTensor = softplus(inputTensor, options);
+    const outputTensor = softplus(inputTensor);
     utils.checkValue(outputTensor, expected);
   }
 
@@ -21,20 +21,6 @@ describe('test softplus', function() {
           1.3132616875182228,
           2.1269280110429727,
         ],
-    );
-  });
-
-  it('softplus', function() {
-    testElu(
-        [4],
-        [-1, 0, 1, 2],
-        [
-          0.0634640055214863,
-          0.34657359027997264,
-          1.0634640055214863,
-          2.0090749639589047,
-        ],
-        {steepness: 2},
     );
   });
 });
