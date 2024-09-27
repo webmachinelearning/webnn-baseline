aab0675bdb053f0ffeabf47952598f6352404bb8
mei1127 <shanxing.mei@intel.com>
2024-09-27 14:42:15 +0800

    revised again

--- src/cumulativeSum.js
+++ src/cumulativeSum.js
@@ -20,19 +20,25 @@ export function cumulativeSum(input, axis, {exclusive = false, reverse = false} 
   const inputElementStart = reverse ? elementCountAlongAxis - 1 : 0;
   const inputElementStep = reverse ? -1 : 1;
   const cumulativeSums = new Array(elementCountAlongAxis).fill(0);
+
   for (let outputIndex = 0; outputIndex < totalElements; outputIndex++) {
     const location = output.locationFromIndex(outputIndex);
     const inputLocation = [...location];
-    const outputLocation = [...location];
+
     for (let i = 0; i < elementCountAlongAxis; ++i) {
-      const idx = inputElementStart + i * inputElementStep;
-      inputLocation[axis]=idx;
-      outputLocation[axis] = idx;
+      const index = inputElementStart + i * inputElementStep;
+      inputLocation[axis] = index;
       const inputValue = input.getValueByLocation(inputLocation);
       cumulativeSums[i] = (i === 0 ? 0 : cumulativeSums[i-1]) + inputValue;
+    }
+
+    for (let i = 0; i < elementCountAlongAxis; ++i) {
+      const index = inputElementStart + i * inputElementStep;
+      location[axis] = index;
       const outputValue = exclusive ? (i === 0 ? 0 : cumulativeSums[i-1]) : cumulativeSums[i];
-      output.setValueByLocation(outputLocation, outputValue);
+      output.setValueByLocation(location, outputValue);
     }
   }
+
   return output;
 }
