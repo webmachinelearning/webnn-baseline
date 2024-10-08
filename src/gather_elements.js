dcda4fa62bc699d6b1dd283d1a61ac444e16e68b
BruceDai <feng.dai@intel.com>
2024-10-08 09:45:51 +0800

    fixed typo and optimized code

--- src/gather_elements.js
+++ src/gather_elements.js
@@ -14,14 +14,15 @@ export function gatherElements(input, indices, {axis = 0} = {}) {
   validateGatherElementsParams(...arguments);
   const outputShape = indices.shape.slice();
   const output = new Tensor(outputShape);
+  const count = sizeOfShape(indices.shape);
 
-  for (let outputIndex = 0; outputIndex < sizeOfShape(outputShape); ++outputIndex) {
+  for (let outputIndex = 0; outputIndex < count; ++outputIndex) {
     const outputLocation = output.locationFromIndex(outputIndex);
     const indicesLocation = outputLocation.slice();
-    let indiceVlaue = indices.getValueByLocation(indicesLocation);
-    indiceVlaue = indiceVlaue < 0 ? indiceVlaue + input.shape[axis] : indiceVlaue;
+    let indicesVlaue = indices.getValueByLocation(indicesLocation);
+    indicesVlaue = indicesVlaue < 0 ? indicesVlaue + input.shape[axis] : indicesVlaue;
     const selectedInputLoc =
-        [...outputLocation.slice(0, axis), indiceVlaue, ...outputLocation.slice(axis + 1)];
+        [...outputLocation.slice(0, axis), indicesVlaue, ...outputLocation.slice(axis + 1)];
     const inputValue = input.getValueByLocation(selectedInputLoc);
     output.setValueByIndex(outputIndex, inputValue);
   }
