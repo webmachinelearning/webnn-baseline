3eee6edd2a635e3286d58ff25e62a39be8be9035
BruceDai <feng.dai@intel.com>
2024-12-13 13:23:46 +0800

    support strides options for slice op

--- src/slice.js
+++ src/slice.js
@@ -8,17 +8,25 @@ import {validateSliceParams} from './lib/validate-input.js';
  * @param {Tensor} input
  * @param {Array} starts
  * @param {Array} sizes
+ * @param {MLSliceOptions} options
  * @return {Tensor}
  */
-export function slice(input, starts, sizes) {
+export function slice(input, starts, sizes, {strides} = {}) {
   validateSliceParams(...arguments);
-  const outputShape = sizes;
+  strides = strides ?? new Array(input.rank).fill(1);
+  const outputShape = input.shape.slice();
+
+  for (let dimensionIndex = 0; dimensionIndex < input.rank; ++dimensionIndex) {
+    outputShape[dimensionIndex] = Math.floor(sizes[dimensionIndex] / strides[dimensionIndex]) +
+      Number(sizes[dimensionIndex] % strides[dimensionIndex]);
+  }
+
   const output = new Tensor(outputShape);
   for (let outputIndex = 0; outputIndex < sizeOfShape(outputShape); ++outputIndex) {
     const loc = output.locationFromIndex(outputIndex);
     const selectedInputLoc = loc.slice();
     for (let i = 0; i < loc.length; ++i) {
-      selectedInputLoc[i] = loc[i] + starts[i];
+      selectedInputLoc[i] = starts[i] + loc[i] * strides[i];
     }
     const inputValue = input.getValueByLocation(selectedInputLoc);
     output.setValueByIndex(outputIndex, inputValue);
