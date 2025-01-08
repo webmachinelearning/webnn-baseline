0ae6903125b22bed9091a9b56132f418b4c052c3
BruceDai <feng.dai@intel.com>
2025-01-08 18:41:29 +0800

    refine implementation of lstm

--- src/lstm.js
+++ src/lstm.js
@@ -3,10 +3,8 @@
 import {concat} from './concat.js';
 import {lstmCell} from './lstm_cell.js';
 import {reshape, squeeze} from './reshape.js';
-import {reverse} from './reverse.js';
 import {sizeOfShape, Tensor} from './lib/tensor.js';
 import {sigmoid} from './sigmoid.js';
-import {split} from './split.js';
 import {slice} from './slice.js';
 import {tanh} from './tanh.js';
 import {validateLstmParams} from './lib/validate-input.js';
@@ -49,12 +47,17 @@ export function lstm(input, weight, recurrentWeight, steps, hiddenSize,
         initialCellState, new Array(sizeOfShape(initialCellState)).fill(0));
   }
 
-  let sequence;
   const currentWeight = [];
   const currentRecurrentWeight = [];
   const currentBias = [];
   const currentRecurrentBias = [];
   const currentPeepholeWeight = [];
+  let forwardSequence = null;
+  let backwardSequence = null;
+  let currentHidden;
+  let currentCell;
+  let outputHidden;
+  let outputCell;
 
   for (let dir = 0; dir < numDirections; ++dir) {
     currentWeight.push(squeeze(slice(weight, [dir, 0, 0], [1, 4 * hiddenSize, inputSize])));
@@ -65,68 +68,60 @@ export function lstm(input, weight, recurrentWeight, steps, hiddenSize,
       (squeeze(slice(recurrentBias, [dir, 0], [1, 4 * hiddenSize]))) : null);
     currentPeepholeWeight.push(peepholeWeight ?
       (squeeze(slice(peepholeWeight, [dir, 0], [1, 3 * hiddenSize]))) : null);
-  }
 
-  for (let step = 0; step < steps; ++step) {
-    const currentHidden = [];
-    const currentCell = [];
-    let nextHidden = null;
-    let nextCell = null;
+    currentHidden = squeeze(slice(hiddenState, [dir, 0, 0], [1, batchSize, hiddenSize]));
+    currentCell = squeeze(slice(cellState, [dir, 0, 0], [1, batchSize, hiddenSize]));
 
-    for (let dir = 0; dir < numDirections; ++dir) {
-      currentHidden.push(squeeze(slice(hiddenState, [dir, 0, 0], [1, batchSize, hiddenSize])));
-      currentCell.push(squeeze(slice(cellState, [dir, 0, 0], [1, batchSize, hiddenSize])));
-    }
-
-    for (let dir = 0; dir < numDirections; ++dir) {
-      const slice0 = (dir == 1 || direction == 'backward' ? steps - step - 1 : step);
+    for (let step = 0; step < steps; ++step) {
+      const slice0 = dir === 1 || direction === 'backward' ? steps - step - 1 : step;
       const currentInput = squeeze(slice(input, [slice0, 0, 0], [1, batchSize, inputSize]));
 
-      const results = lstmCell(
+      [currentHidden, currentCell] = lstmCell(
           currentInput, currentWeight[dir], currentRecurrentWeight[dir],
-          currentHidden[dir], currentCell[dir], hiddenSize, {bias: currentBias[dir],
+          currentHidden, currentCell, hiddenSize, {bias: currentBias[dir],
             recurrentBias: currentRecurrentBias[dir], peepholeWeight: currentPeepholeWeight[dir],
             layout: layout, activations: activations});
-      // Expand [batchSize, hiddenSize] to [numDirections, batchSize, hiddenSize]
-      const output = reshape(results[0], [1, batchSize, hiddenSize]);
-      const cell = reshape(results[1], [1, batchSize, hiddenSize]);
 
-      // Concat along axis 0 (numDirections dimension)
-      nextHidden = (nextHidden ? concat([nextHidden, output], 0) : output);
-      nextCell = (nextCell ? concat([nextCell, cell], 0) : cell);
+      if (returnSequence) {
+        // Expand hidden of 2D([batchSize, hiddenSize]) to
+        // 4D([steps, numDirections, batchSize, hiddenSize])
+        const expandedHiddenAs4D = reshape(currentHidden, [1, 1, batchSize, hiddenSize]);
+        if (direction === 'forward' || (dir === 0 && direction === 'both')) {
+          forwardSequence = forwardSequence ?
+              concat([forwardSequence, expandedHiddenAs4D], 0) :
+              expandedHiddenAs4D;
+        } else if (direction === 'backward' || (dir === 1 && direction === 'both')) {
+          backwardSequence = backwardSequence ?
+              concat([expandedHiddenAs4D, backwardSequence], 0) :
+              expandedHiddenAs4D;
+        }
+      }
     }
 
-    hiddenState = nextHidden;
-    cellState = nextCell;
+    // Expand hidden of 2D([batchSize, hiddenSize]) to 3D([numDirections, batchSize, hiddenSize])
+    const expandHiddenAs3D = reshape(currentHidden, [1, batchSize, hiddenSize]);
+    // Concat along axis 0 (numDirections dimension)
+    outputHidden = outputHidden ? concat([outputHidden, expandHiddenAs3D], 0) : expandHiddenAs3D;
 
-    if (returnSequence) {
-      // Expand [numDirections, batchSize, hiddenSize] to
-      // [steps, numDirections, batchSize, hiddenSize]
-      nextHidden = reshape(nextHidden, [1, numDirections, batchSize, hiddenSize]);
-      // Concat output sequence along axis 0 (steps dimension)
-      sequence = (sequence ? concat([sequence, nextHidden], 0) : nextHidden);
-    }
+    // Expand cell of 2D([batchSize, hiddenSize]) to 3D([numDirections, batchSize, hiddenSize])
+    const expandCellAs3D = reshape(currentCell, [1, batchSize, hiddenSize]);
+    // Concat along axis 0 (numDirections dimension)
+    outputCell = outputCell ? concat([outputCell, expandCellAs3D], 0) : expandCellAs3D;
   }
 
   if (returnSequence) {
-    if (direction === 'backward') {
-      // Refer to https://www.w3.org/TR/webnn/#api-mlgraphbuilder-lstm, Spec says the
-      // sequence should contain every output from each time step in the temporal sequence, while
-      // the loop for steps concatenates sequence in a reversed order when direction is backward,
-      // so here need reverse output sequence along axis 0 (steps dimension).
-      sequence = reverse(sequence, {axes: [0]});
+    // outputSequence: [steps, numDirections, batchSize, hiddenSize]
+    let outputSequence;
+    if (direction === 'forward') {
+      outputSequence = forwardSequence;
+    } else if (direction === 'backward') {
+      outputSequence = backwardSequence;
     } else if (direction === 'both') {
-      // Split output sequence into forward-sequence and backward-sequence two sequences along axis
-      // 1 (numDirections dimension)
-      const [sequenceForward, sequenceBackward] = split(sequence, 2, {axis: 1});
-      // Reverse backward-sequence along axis 0 (steps dimension)
-      const reversedSequenceBackward = reverse(sequenceBackward, {axes: [0]});
-      sequence = concat([sequenceForward, reversedSequenceBackward], 1);
-    } else {
-      // No need update sequence for 'forward' direction
+      // Concat along axis 1 (numDirections dimension)
+      outputSequence = concat([forwardSequence, backwardSequence], 1);
     }
-    return [hiddenState, cellState, sequence];
+    return [outputHidden, outputCell, outputSequence];
   } else {
-    return [hiddenState, cellState];
+    return [outputHidden, outputCell];
   }
 }
